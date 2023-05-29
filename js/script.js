4693fff500f0785b860bcfebf972ec10822e6fdb
alyssia-fung <124389558+alyssia-fung@users.noreply.github.com>
2023-05-29 17:17:02 +0000

    sync

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+"use strict"
+
+/**
+ * This function does volume of a sphere
+ */
+window.onload = function () {
+  // this calculates volume of a sphere
+
+  const params = new URLSearchParams(document.location.search)
+
+  // input
+  const radius = params.get("r")
+  console.log(radius)
+
+  // process
+  const volume = (4 / 3) * Math.PI * radius ** 3
+  const roundedVolume = volume.toFixed(2)
+
+  // output
+  document.getElementById("dimension").innerHTML = "r = " + radius
+  document.getElementById("output").innerHTML =
+    "Volume: " + roundedVolume + " cm³"
+}
